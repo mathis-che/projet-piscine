fa3ce484a16116f1eca6f67b6082e985539a1cc1
mathis-che <132149694+mathis-che@users.noreply.github.com>
2024-06-02 13:50:03 +0200

    Add files via upload

---
+++ tout_parcourir.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <title>Tout parcourir</title>
+    <link rel="stylesheet" href="agora.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <h1>Agora Francia</h1>
+            <img src="logo.png" alt="Logo du site" class="logo">
+        </header>
+        <nav>
+            <ul>
+                <li><a href="accueil.php">Accueil</a></li>
+                <li><a href="tout_parcourir.php" class="active">Tout parcourir</a></li>
+                <li><a href="notifications.php">Notifications</a></li>
+                <li><a href="panier.php">Panier</a></li>
+                <li><a href="votre_compte.php">Votre compte</a></li>
+            </ul>
+        </nav>
+        <section>
+            
+        </section>
+
+        <footer>
+            <ul>
+                <ul>Contact: <a href="mailto:info@agorafrancia.fr">info@agorafrancia.fr</a></ul>
+                <ul>Tel : 01.02.03.04.05</ul>
+                <ul>Adresse : 37 Quai de Grenelle </ul>
+            </ul>
+            <ul>
+                <ul>&copy; 2024 Agora Francia</ul>
+            </ul>
+        </footer>
+        
+    </div>
+</body>
+</html>
